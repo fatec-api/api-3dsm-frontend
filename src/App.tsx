ac76b9c04d3aab42bc1b69244db87d73880136ed
mariaflbss <mariaf.laboissiere@gmail.com>
2026-03-18 17:10:07 -0300

    feat(US01-04):adicionando a rota /telalogin

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,31 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Input from './shared/components/Input'
 import Header from './shared/components/Header'
 import Home from './pages/Home'
+import TelaLogin from './pages/TelaLogin'
+
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />}/>
-      <Route path='login' element={<Input/>}/>
-    </Route>
-  )
+ createRoutesFromElements(
+   <>
+     <Route path="/" element={<Header />}>
+       <Route index element={<Home />}/>
+       <Route path='/login' element={<Input/>}/>
+     </Route>
+     <Route path='/telalogin' element={<TelaLogin/>}/>
+   </>
+ )
 )
+
 
 function App() {
 
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+
+ return (
+   <>
+     <RouterProvider router={router} />
+   </>
+ )
 }
+
 
 export default App
