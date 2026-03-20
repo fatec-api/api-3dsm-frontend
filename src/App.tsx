000a64d18eab715b2d1ef571467ba246ba529c24
mariaflbss <mariaf.laboissiere@gmail.com>
2026-03-20 18:18:22 -0300

    feat(US01-06): Adicionando tela de Cadastro do usuário e adicionando rota no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import { useState } from 'react'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Input from './components/Input'
-import Header from './shared/components/Header'
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CadastroUsuario from "./pages/TelaCadastroUsuario";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />}/>
-      <Route path='login' element={<Input/>}/>
-    </Route>
-  )
-)
 
 function App() {
-
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+ return (
+   <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<CadastroUsuario />} />
+     </Routes>
+   </BrowserRouter>
+ );
 }
 
-export default App
+
+export default App;
