3ee95400f9360be2ee4112bbb9aa224d4db76d05
mariaflbss <mariaf.laboissiere@gmail.com>
2026-03-23 15:17:48 -0300

    style(US01-06): Estilização do tamanho do botão submit do formulário de cadastro do usuário

--- src/shared/components/Botao.tsx
+++ src/shared/components/Botao.tsx
@@ -7,7 +7,7 @@ export default function Botao({ children, type = "button" }: Props) {
   return (
     <button
       type={type}
-      className="border-2 border-black rounded-xl h-10 mt-2 bg-white hover:bg-gray-100"
+      className="border-2 border-black rounded-xl h-10 px-6 yx-3 mt-2 bg-white hover:bg-gray-100"
     >
       {children}
     </button>
