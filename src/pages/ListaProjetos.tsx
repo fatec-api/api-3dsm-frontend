96568d175dbb46ec5c88e7be11854201a004ddb3
Rebeca Lima <becaln.ti0@gmail.com>
2026-03-28 14:18:32 -0300

    feat(US02-05): Implementação do Axios para futura integração com o backend

--- src/pages/ListaProjetos.tsx
+++ src/pages/ListaProjetos.tsx
@@ -1,72 +1,42 @@
+import { useEffect, useState } from "react";
 import Header from "../shared/components/Header"
+import { listarProjetos } from "../services/listService";
 
 export default function ListaProjetos() {
 
-    // dados mockados para simular os projetos
-    const projetos = [
-        {
-            nomeProjeto: "Projeto A",
-            tipoProjeto: "Alocação",
-            status: "Em andamento"
-        },
-        {
-            nomeProjeto: "Projeto B",
-            tipoProjeto: "Hora Fechada",
-            status: "Em andamento"
-        },{
-            nomeProjeto: "Projeto C",
-            tipoProjeto: "Alocação",
-            status: "Concluído"
-        },
-        {
-            nomeProjeto: "Projeto D",
-            tipoProjeto: "Hora Fechada",
-            status: "Em andamento"
-        },{
-            nomeProjeto: "Projeto W",
-            tipoProjeto: "Alocação",
-            status: "Planejamento"
-        },
-        {
-            nomeProjeto: "Projeto X",
-            tipoProjeto: "Hora Fechada",
-            status: "Planejamento"
-        },{
-            nomeProjeto: "Projeto Y",
-            tipoProjeto: "Alocação",
-            status: "Em andamento"
-        },
-        {
-            nomeProjeto: "Projeto Z",
-            tipoProjeto: "Hora Fechada",
-            status: "Concluído"
-        },
-    ]
+    const [projetos, setProjetos] = useState<{nomeProjeto: string, tipoProjeto: string, status: string}[]>([]);
 
-    function renderizarProjetos() {
-        if (projetos.length === 0) {
-            return (
-                <div role="alert" className="alert alert-info alert-soft h-15">
-                    <p className="text-lg">Nenhum projeto encontrado.</p>
-                </div>
-            )
-        } else {
-            return projetos.map((projeto, index) => (
-                <div key={index} className="flex flex-col justify-between border hover:shadow-lg hover:transform hover:scale-105 hover:transition hover:duration-300 p-6 rounded-lg h-50 w-64">
-                    <h2 className="text-center text-xl font-bold mb-2">{projeto.nomeProjeto}</h2>
-                    <div>
-                        <p className="text-gray-600">Tipo: {projeto.tipoProjeto}</p>
-                        <p className="text-gray-600">Status: {projeto.status}</p>
-                    </div>
-                </div>
-            ))
-        }
-    }
+      useEffect(() => {
+        const loadData = async () => {
+            try {
+                const listaProjetos = await listarProjetos();
+                setProjetos(listaProjetos);
+            } catch (error) {
+                console.error("Erro ao carregar dados", error);
+            }
+        };
+        loadData();
+    }, []);
+
     return (
         <div className="flex h-screen bg-[#FFFFFF]">
             <Header />
             <div className="flex mt-40 gap-6 w-full justify-center flex-wrap">
-                {renderizarProjetos()}
+                {projetos.length === 0 ? (
+                    <div role="alert" className="alert alert-info alert-soft h-15">
+                        <p className="text-lg">Nenhum projeto encontrado.</p>
+                    </div>
+                ) : (
+                    projetos.map((projeto, index) => (
+                        <div key={index} className="flex flex-col justify-between border hover:shadow-lg hover:transform hover:scale-105 hover:transition hover:duration-300 p-6 rounded-lg h-50 w-64">
+                            <h2 className="text-center text-xl font-bold mb-2">{projeto.nomeProjeto}</h2>
+                            <div>
+                                <p className="text-gray-600">Tipo: {projeto.tipoProjeto}</p>
+                                <p className="text-gray-600">Status: {projeto.status}</p>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     );
