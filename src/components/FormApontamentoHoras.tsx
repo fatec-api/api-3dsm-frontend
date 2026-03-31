68617833eb3e91173f7dd7ae64c932ee137803a0
mariaflbss <mariaf.laboissiere@gmail.com>
2026-03-31 17:59:39 -0300

    feat(US06-02): Adicionando estados iniciais do formulário de apontamento de horas e conectando os inputs aos estados do formulário

--- src/components/FormApontamentoHoras.tsx
+++ src/components/FormApontamentoHoras.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import Input from "../shared/components/Input";
 import Dropdown from "../shared/components/Dropdown";
 import Botao from "../shared/components/Botao";
 
 export default function FormularioApontamento() {
+
+  const [projeto, setProjeto] = useState("");
+  const [item, setItem] = useState("");
+  const [nivel, setNivel] = useState("");
+  const [data, setData] = useState("");
+  const [horaInicio, setHoraInicio] = useState("");
+  const [horaFim, setHoraFim] = useState("");
+
   return (
     <form className="bg-white shadow-lg rounded-2xl p-10 w-full max-w-[700px] flex flex-col gap-10">
       
@@ -11,20 +20,49 @@ export default function FormularioApontamento() {
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <Dropdown label="Projeto" options={[]} />
-        <Dropdown label="Item" options={[]} />
+        <Dropdown
+          label="Projeto"
+          value={projeto}
+          onChange={(e: any) => setProjeto(e.target.value)}
+          options={[]}
+        />
+
+        <Dropdown
+          label="Item"
+          value={item}
+          onChange={(e: any) => setItem(e.target.value)}
+          options={[]}
+        />
       </div>
 
       <Dropdown
         label="Nível da Atividade"
+        value={nivel}
+        onChange={(e: any) => setNivel(e.target.value)}
         options={["Análise", "Desenvolvimento", "Teste"]}
       />
 
-      <Input label="Data" type="date" />
+      <Input
+        label="Data"
+        type="date"
+        value={data}
+        onChange={(e: any) => setData(e.target.value)}
+      />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <Input label="Hora Início" type="time" />
-        <Input label="Hora Fim" type="time" />
+        <Input
+          label="Hora Início"
+          type="time"
+          value={horaInicio}
+          onChange={(e: any) => setHoraInicio(e.target.value)}
+        />
+
+        <Input
+          label="Hora Fim"
+          type="time"
+          value={horaFim}
+          onChange={(e: any) => setHoraFim(e.target.value)}
+        />
       </div>
 
       <div className="flex flex-col gap-4">
