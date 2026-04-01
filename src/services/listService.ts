83ae7a9e606783d7958d68c5cee53a8ea76939b6
Rebeca Lima <becaln.ti0@gmail.com>
2026-04-01 10:07:53 -0300

    feat(US02-05): Integração com o backend

--- src/services/listService.ts
+++ src/services/listService.ts
@@ -1,43 +1,6 @@
 import api from "./api";
 
-export function listarProjetos() {
-    return Promise.resolve([
-        {
-            nomeProjeto: "Projeto A",
-            tipoProjeto: "Alocação",
-            status: "Em andamento"
-        },
-        {
-            nomeProjeto: "Projeto B",
-            tipoProjeto: "Hora Fechada",
-            status: "Em andamento"
-        },{
-            nomeProjeto: "Projeto C",
-            tipoProjeto: "Alocação",
-            status: "Concluído"
-        },
-        {
-            nomeProjeto: "Projeto D",
-            tipoProjeto: "Hora Fechada",
-            status: "Em andamento"
-        },{
-            nomeProjeto: "Projeto W",
-            tipoProjeto: "Alocação",
-            status: "Planejamento"
-        },
-        {
-            nomeProjeto: "Projeto X",
-            tipoProjeto: "Hora Fechada",
-            status: "Planejamento"
-        },{
-            nomeProjeto: "Projeto Y",
-            tipoProjeto: "Alocação",
-            status: "Em andamento"
-        },
-        {
-            nomeProjeto: "Projeto Z",
-            tipoProjeto: "Hora Fechada",
-            status: "Concluído"
-        },
-    ])
+export async function listarProjetos() {
+    const response = await api.get("/listar/projetos")
+    return response.data
 }
