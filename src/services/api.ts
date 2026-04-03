5537e9f9397f7249f160320aeeeb460f3640cbc3
Gustavo Rosa <gustavorib.rosa@gmail.com>
2026-04-03 01:41:44 -0300

    refactor(US06-02): Removendo os dados mockados e adicionado a integração dos itens do projeto

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // futuro backend
+  baseURL: "http://localhost:8080/", // futuro backend
   timeout: 5000,
 });
 
