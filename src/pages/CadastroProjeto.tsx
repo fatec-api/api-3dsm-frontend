a9164c375ad7501a89864d44f133f9dd8f303471
Kodatoo <gkodatofaria@gmail.com>
2026-04-05 00:34:21 -0300

    fix(US02-02): Inclusão de chave.

--- src/pages/CadastroProjeto.tsx
+++ src/pages/CadastroProjeto.tsx
@@ -4,7 +4,7 @@ import Input from "../shared/components/Input";
 import { PiHandCoins } from "react-icons/pi";
 import { GoProject } from "react-icons/go";
 import Dropdown from "../shared/components/Dropdown";
-import { listarProfissionais, listarClientes, criarProjeto } from "../services/projectService";
+import { listarProfissionaisAtivos, listarClientes, criarProjeto, listarUsuariosAtivos } from "../services/projectService";
 
 export default function CadastroProjeto() {
     const [alerta, setAlerta] = useState("");
@@ -17,18 +17,26 @@ export default function CadastroProjeto() {
     const [statusProjeto, setStatusProjeto] = useState("");
     const [profissionalAlocado, setProfissionalAlocado] = useState<string[]>([]);
     const [gestorResponsavel, setGestorResponsavel] = useState("");
-    const [profissionais, setProfissionais] = useState<{ nomeProfissional: string; cargo: string }[]>([]);
+    const [profissionais, setProfissionais] = useState<{ nomeUsuario: string; cargo: string }[]>([]);
     const [clientes, setClientes] = useState<string[]>([]);
-    const [gestores, setGestores] = useState<string[]>([]);
+    const [gestores, setGestores] = useState<{ label: string; value: string }[]>([]);
 
     const regex = /^[A-Z]{3}\d{4}$/;
 
     useEffect(() => {
         const loadData = async () => {
             try {
-                const listaProfissionais = await listarProfissionais();
+                const listaProfissionais = await listarProfissionaisAtivos();
                 setProfissionais(listaProfissionais);
-                setGestores(listaProfissionais.filter((p: { cargo: string; }) => p.cargo == "Gestor").map((p: { nomeProfissional: string; }) => p.nomeProfissional));
+                const listaUsuarios = await listarUsuariosAtivos();
+                const gestoresMapeados = listaUsuarios
+                    .filter((p: any) => p.cargo === "Gestor")
+                    .map((p: any) => ({
+                        label: p.nomeUsuario,
+                        value: String(p.id)          
+                    }));
+
+                setGestores(gestoresMapeados);
                 const listaClientes = await listarClientes();
                 setClientes(listaClientes.map((c: { nomeCliente: string; }) => c.nomeCliente));
             } catch (error) {
@@ -78,10 +86,11 @@ export default function CadastroProjeto() {
                     valorOrcamento: Number(valorOrcamento),
                     dataInicio,
                     dataFim,
-                    statusProjeto,
+                    status: statusProjeto,
                     profissionalAlocado,
-                    gestorResponsavel,
+                    gestorId: gestorResponsavel,
                 };
+                console.log(payload)
 
                 await criarProjeto(payload);
                 setAlerta("Projeto cadastrado com sucesso!");
@@ -99,110 +108,121 @@ export default function CadastroProjeto() {
             }
         }
     }
-
     return (
-        <div className="flex h-screen bg-[#FFFFFF]">
+        <div className="h-screen flex flex-col bg-white">
             <Header />
-            <form className="flex flex-col items-center w-full mt-40" onSubmit={handleCadastro}>
-                <h1 className="form-title text-2xl font-bold mb-5">Cadastro de Projeto</h1>
-                <div className="join join-vertical lg:join-horizontal">
-                    <div className="join join-vertical px-8">
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Nome do Projeto</label>
-                            <Input
-                                type="text"
-                                placeholder="Ex: ABC1234"
-                                value={nomeProjeto}
-                                onChange={(e) => setNomeProjeto(e.target.value.toUpperCase())}
-                                icon={<GoProject size={20} />}
-                                required
-                            />
+            <div className="flex-1 flex items-center justify-center px-4">
+                <form className="bg-white shadow-lg rounded-2xl p-10 w-fit flex flex-col gap-8" onSubmit={handleCadastro}>
+                    <h1 className="form-title text-2xl font-bold mb-5 text-center">Cadastro de Projeto</h1>
+                    <div className="join join-vertical lg:join-horizontal">
+                        <div className="join join-vertical px-8">
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Nome do Projeto</label>
+                                <Input
+                                    type="text"
+                                    placeholder="Ex: ABC1234"
+                                    value={nomeProjeto}
+                                    onChange={(e) => setNomeProjeto(e.target.value.toUpperCase())}
+                                    icon={<GoProject size={20} />}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Tipo de Projeto</label>
+                                <Dropdown
+                                    value={tipoProjeto}
+                                    onChange={(e) => setTipoProjeto(e.target.value)}
+                                    options={[
+                                        { label: "Alocação", value: "Alocacao" },
+                                        { label: "Hora Fechada", value: "Hora_Fechada" }
+                                    ]}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Cliente</label>
+                                <Dropdown
+                                    value={cliente}
+                                    onChange={(e) => setCliente(e.target.value)}
+                                    options={clientes}
+                                    required={false}
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Valor do Orçamento</label>
+                                <Input
+                                    type="double"
+                                    placeholder="R$00,00"
+                                    value={valorOrcamento}
+                                    onChange={(e) => setValorOrcamento(e.target.value)}
+                                    icon={<PiHandCoins size={20} />}
+                                    required
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Tipo de Projeto</label>
-                            <Dropdown
-                                value={tipoProjeto}
-                                onChange={(e) => setTipoProjeto(e.target.value)}
-                                options={['Alocação', 'Hora Fechada']}
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Cliente</label>
-                            <Dropdown
-                                value={cliente}
-                                onChange={(e) => setCliente(e.target.value)}
-                                options={clientes}
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Valor do Orçamento</label>
-                            <Input
-                                type="double"
-                                placeholder="R$00,00"
-                                value={valorOrcamento}
-                                onChange={(e) => setValorOrcamento(e.target.value)}
-                                icon={<PiHandCoins size={20} />}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="join join-vertical px-8">
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Data de Início</label>
-                            <Input
-                                type="date"
-                                placeholder="dd/mm/aaaa"
-                                value={dataInicio}
-                                onChange={(e) => setDataInicio(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Data de Fim</label>
-                            <Input
-                                type="date"
-                                placeholder="dd/mm/aaaa"
-                                value={dataFim}
-                                onChange={(e) => setDataFim(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Status do Projeto</label>
-                            <Dropdown
-                                value={statusProjeto}
-                                onChange={(e) => setStatusProjeto(e.target.value)}
-                                options={['Planejamento', 'Andamento', 'Concluído']}
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block ms-3 mb-1 font-medium">Profissionais</label>
-                            <div className="flex items-center border-2 border-gray-300 rounded-xl px-3 bg-white focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-200 transition duration-200">
-                            <a href="#modal-profissional" className="flex-1 outline-none bg-transparent text-gray-700 appearance-none cursor-pointer py-2.5">
-                                {profissionalAlocado.length > 0 ? `${profissionalAlocado.length} selecionado(s)` : "Selecione"}
-                            </a>
-                        </div>
-                        </div>
-                        
-                    </div>
-                </div>
-                <div className="mb-5">
-                    <label className="block ms-3 mb-1 font-medium">Gestor Responsável</label>
-                    <Dropdown
-                        value={gestorResponsavel}
-                        onChange={(e) => setGestorResponsavel(e.target.value)}
-                        options={gestores}
-                        required
-                    />
-                </div>
-                {alerta && <p className={`mb-4 text-center ${alerta.includes("sucesso") ? "alert alert-outline alert-success text-sm bg-green-100" : "alert alert-outline alert-error text-sm bg-red-100"}`}>{alerta}</p>}
+                        <div className="join join-vertical px-8">
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Data de Início</label>
+                                <Input
+                                    type="date"
+                                    placeholder="dd/mm/aaaa"
+                                    value={dataInicio}
+                                    onChange={(e) => setDataInicio(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Data de Fim</label>
+                                <Input
+                                    type="date"
+                                    placeholder="dd/mm/aaaa"
+                                    value={dataFim}
+                                    onChange={(e) => setDataFim(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Status do Projeto</label>
+                                <Dropdown
+                                    value={statusProjeto}
+                                    onChange={(e) => setStatusProjeto(e.target.value)}
+                                    options={[
+                                        {label: "Desenvolvimento", value: "Desenvolvimento"},
+                                        {label:"Andamento", value: "Andamento"},
+                                        {label: "Concluído", value: "Concluida"}
+                                    ]}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block ms-3 mb-1 font-medium">Profissionais</label>
+                                <div className="flex items-center border-2 border-gray-300 rounded-xl px-3 bg-white focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-200 transition duration-200">
+                                    <a href="#modal-profissional" className="flex-1 outline-none bg-transparent text-gray-700 appearance-none cursor-pointer py-2.5">
+                                        {profissionalAlocado.length > 0 ? `${profissionalAlocado.length} selecionado(s)` : "Selecione"}
+                                    </a>
+                                </div>
+                            </div>
 
-                <button type="submit" className="border-2 border-black rounded-xl bg-white hover:bg-gray-100 p-3 m-2">
-                    Cadastrar
-                </button>
-            </form>
+                        </div>
+                    </div>
+                    <div className="px-8 w-full flex justify-center">
+                        <div className="mb-5 w-full max-w-xs">
+                            <label className="block ms-3 mb-1 font-medium">Gestor Responsável</label>
+                            <Dropdown
+                                value={gestorResponsavel}
+                                onChange={(e) => setGestorResponsavel(e.target.value)}
+                                options={gestores}
+                                required
+                            />
+                        </div>
+                    </div>
+                    {alerta && <p className={`mb-4 text-center ${alerta.includes("sucesso") ? "alert alert-outline alert-success text-sm bg-green-100" : "alert alert-outline alert-error text-sm bg-red-100"}`}>{alerta}</p>}
+
+                    <button type="submit" className="border-2 border-black rounded-xl w-xs bg-white hover:bg-gray-100 p-3 m-2 self-center">
+                        Cadastrar
+                    </button>
+                </form>
+            </div>
 
             <div className="modal" role="dialog" id="modal-profissional">
                 <div className="modal-box">
@@ -213,18 +233,18 @@ export default function CadastroProjeto() {
                                 <li className="alert alert-soft alert-info text-lg p-2">Sem profissionais para alocação.</li>
                             ) : (
                                 profissionais.map((profissional) => (
-                                <li key={profissional.nomeProfissional} className="list-row flex items-center gap-3 p-2">
-                                    <input
-                                        type="checkbox"
-                                        className="checkbox"
-                                        checked={profissionalAlocado.includes(profissional.nomeProfissional)}
-                                        onChange={() => toggleProfissional(profissional.nomeProfissional)}
-                                    />
-                                    <div className="flex-1">
-                                        <h2 className="font-bold">{profissional.nomeProfissional}</h2>
-                                        <p className="text-sm text-gray-600">{profissional.cargo}</p>
-                                    </div>
-                                </li>
+                                    <li key={profissional.nomeUsuario} className="list-row flex items-center gap-3 p-2">
+                                        <input
+                                            type="checkbox"
+                                            className="checkbox"
+                                            checked={profissionalAlocado.includes(profissional.nomeUsuario)}
+                                            onChange={() => toggleProfissional(profissional.nomeUsuario)}
+                                        />
+                                        <div className="flex-1">
+                                            <h2 className="font-bold">{profissional.nomeUsuario}</h2>
+                                            <p className="text-sm text-gray-600">{profissional.cargo}</p>
+                                        </div>
+                                    </li>
                                 ))
                             )}
                         </ul>
