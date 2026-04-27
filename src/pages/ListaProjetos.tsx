c683ad4f3d1cceb2090c9020eb611be634207c71
Rebeca Lima <becaln.ti0@gmail.com>
2026-04-27 16:44:29 -0300

    feat(US10-17): Conclusão da implementação de filtros para listagem de projetos e testes com dados mockados

--- src/pages/ListaProjetos.tsx
+++ src/pages/ListaProjetos.tsx
@@ -5,11 +5,13 @@ import { listarProjetos } from "../services/projectService";
 import { FaChevronRight } from "react-icons/fa";
 import FiltrosListagemProjetos from "../components/FiltrosListagemProjetos";
 
-interface Projeto {
+export interface Projeto {
     id: number;
     nomeProjeto: string;
+    gestorId: string;
     tipoProjeto: string;
     status: string;
+    cliente: string;
     horasPrevistasTotal: number;
     horasRealizadasTotal: number;
     horasPendentesTotal?: number;
@@ -21,9 +23,50 @@ export default function ListaProjetos() {
     const [projetos, setProjetos] = useState<Projeto[]>([]);
     const [loading, setLoading] = useState(true);
     const [erro, setErro] = useState(false);
+    const [filtros, setFiltros] = useState({
+        nome: "", // Adicionado
+        progresso: "",
+        status: "",
+        tipoProjeto: "",
+        cliente: ""
+    });
 
     const navigate = useNavigate();
 
+    const getProgresso = (projeto: Projeto): string => {
+        if (!projeto.horasPrevistasTotal) return "Sem previsão";
+
+        const percentualConsumo =
+            (projeto.horasRealizadasTotal / projeto.horasPrevistasTotal) * 100;
+
+        if (percentualConsumo <= 75) return "Normal";
+        if (percentualConsumo <= 100) return "Crítico";
+        return "Atrasado";
+    };
+
+    const projetosFiltrados = projetos.filter(projeto => {
+        const progressoProjeto = getProgresso(projeto);
+
+        if (filtros.nome && !projeto.nomeProjeto.toLowerCase().includes(filtros.nome.toLowerCase())) {
+            return false;
+        }
+        if (filtros.progresso && progressoProjeto !== filtros.progresso) return false;
+        if (filtros.status && projeto.status !== filtros.status) return false;
+        if (filtros.tipoProjeto && projeto.tipoProjeto !== filtros.tipoProjeto) return false;
+        if (filtros.cliente && projeto.cliente !== filtros.cliente) return false;
+
+        return true;
+    });
+
+    const handleFilterChange = (novosFiltros: any) => {
+        setFiltros({
+            nome: novosFiltros.nome,
+            progresso: novosFiltros.progresso,
+            status: novosFiltros.statusProjeto,
+            tipoProjeto: novosFiltros.tipoProjeto,
+            cliente: novosFiltros.cliente
+        });
+    };
     useEffect(() => {
         const loadData = async () => {
             try {
@@ -42,9 +85,9 @@ export default function ListaProjetos() {
                 setLoading(false);
             }
         };
- 
+
         loadData();
-    }, []); 
+    }, []);
 
     // regra de cor baseada no percentual
     const getCor = (projeto: Projeto) => {
@@ -72,14 +115,17 @@ export default function ListaProjetos() {
     return (
         <div className="flex flex-col h-screen bg-white">
             <Header />
-            <FiltrosListagemProjetos/>
+            <FiltrosListagemProjetos
+                projetos={projetos}
+                onFilterChange={handleFilterChange}
+            />
             <div className="flex gap-6 w-full justify-center flex-wrap">
-                {projetos.length === 0 ? (
+                {projetosFiltrados.length === 0 ? (
                     <div role="alert" className="alert alert-info alert-soft h-15">
                         <p className="text-lg">Nenhum projeto encontrado.</p>
                     </div>
                 ) : (
-                    projetos.map((projeto) => {
+                    projetosFiltrados.map((projeto) => {
                         const cor = getCor(projeto);
 
                         return (
@@ -107,7 +153,7 @@ export default function ListaProjetos() {
                                                 <b>Tipo:</b> {projeto.tipoProjeto}
                                             </p>
                                             <p>
-                                                <b>Cliente:</b> Cliente
+                                                <b>Cliente:</b> {projeto.cliente}
                                             </p>
                                             <p>
                                                 <b>Status:</b> {projeto.status}
