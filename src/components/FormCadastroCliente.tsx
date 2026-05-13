d03dc1606d75644a514b3f17996490dd84cb98cb
Laís <zanardilais@gmail.com>
2026-05-13 07:54:24 -0300

    feat(US15-01): adiciona validação de cnpj no cadastro de clientes

--- src/components/FormCadastroCliente.tsx
+++ src/components/FormCadastroCliente.tsx
@@ -16,6 +16,42 @@ export default function CadastroCliente() {
   const [sucesso, setSucesso] = useState(false);
   const [carregando, setCarregando] = useState(false);
 
+  const validarCNPJ = (cnpj) => {
+  cnpj = cnpj.replace(/\D/g, "");
+  if (cnpj.length !== 14) return false;
+  if (/^(\d)\1+$/.test(cnpj)) return false;
+
+  let tamanho = cnpj.length - 2;
+  let numeros = cnpj.substring(0, tamanho);
+  const digitos = cnpj.substring(tamanho);
+
+  let soma = 0;
+  let pos = tamanho - 7;
+
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) pos = 9;
+  }
+
+  let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+  if (resultado !== Number(digitos.charAt(0))) return false;
+
+  tamanho += 1;
+  numeros = cnpj.substring(0, tamanho);
+
+  soma = 0;
+  pos = tamanho - 7;
+
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) pos = 9;
+  }
+
+  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+  return resultado === Number(digitos.charAt(1));
+};
+
   const formatarCNPJ = (value) => {
     return value
       .replace(/\D/g, "")
@@ -32,6 +68,14 @@ export default function CadastroCliente() {
     setAlerta("");
 
     const cnpjLimpo = cnpj.replace(/\D/g, "");
+
+     if (!validarCNPJ(cnpjLimpo)) {
+    setSucesso(false);
+    setAlerta("CNPJ inválido.");
+    setCarregando(false);
+    return;
+  }
+
     
     const dados = { 
       razaoSocial: nomeEmpresa, 
